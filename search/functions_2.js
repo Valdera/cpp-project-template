b02918483defda7ecfa1ba372b0ba327da0fe19a
Valdera <>
2023-04-03 16:13:12 +0000

    Valdera published a site update

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['set_11',['set',['../my__lib_2CMakeLists_8txt.html#a20888cd9f3ac69772a0cd8ed0186c1a5',1,'CMakeLists.txt']]]
+  ['set_11',['set',['../my__lib_2CMakeLists_8txt.html#a1eae6299259f4c1883ef119b5f4b0bbc',1,'set(LIBRARY_SOURCES &quot;${CMAKE_CURRENT_SOURCE_DIR}/my_lib.cpp&quot;) set(LIBRARY_HEADERS &quot;$:&#160;CMakeLists.txt'],['../my__lib_2CMakeLists_8txt.html#a3ddbfbe904bdf0d62178df205c3e98ac',1,'set(LIBRARY_INCLUDES &quot;./&quot; &quot;${CMAKE_BINARY_DIR}/configured_files/include&quot;) add_library($:&#160;CMakeLists.txt']]]
 ];
